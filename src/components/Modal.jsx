ea2555fec27c3a7f880510fe9e4ba7552ffa6bc9
Raul-TC <raultc.9819@gmail.com>
2023-02-12 16:40:08 -0600

    finished

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -5,7 +5,7 @@ const Modal = ({ modal, setModal, counterImages }) => {
     const [counterImagesTwo, setCounterImagesTwo] = useState(counterImages);
 
     return (
-        <div className='hidden  md:flex fixed bg-black w-full h-screen bottom-0 right-0 z-10 '>
+        <div className='hidden md:flex bg-black w-full h-screen bottom-0 right-0 z-10 '>
             <div className='w-auto h-auto m-auto flex-col'>
                 <span onClick={() => setModal(!modal)} className=' bg-closeModal hover:bg-closeModalHover bg-center bg-cover z-20 block w-7 h-7 cursor-pointer ml-auto mb-4 '></span>
                 <Slider modal={modal} setModal={setModal} counterImages={counterImagesTwo} setCounterImages={setCounterImagesTwo} />
