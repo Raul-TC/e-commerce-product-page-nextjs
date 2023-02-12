1cd6caad1e08e5eaa15325f3a3b5c7097a4e998d
Raul-TC <raultc.9819@gmail.com>
2023-02-12 17:02:11 -0600

    finished

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -31,7 +31,7 @@ const Header = ({ quantity, menuCart, setMenuCart }) => {
                         {sections.map(sect => <li className='md:hover:border-b-4 border-orange cursor-pointer h-24 leading-[96px]' key={sect}>{sect}</li>)}
                     </ol>
                     <div className={`${menuHamburguer ? '-translate-x-full' : 'translate-x-0'} transition-all ease-linear duration-300 md:hidden fixed w-full left-0 bottom-0 h-screen bg-black z-40`}>
-                        <ol className={`${menuHamburguer ? '-translate-x-full' : 'translate-x-0'} transition-all ease-linear duration-300  absolute top-0 bottom-0 w-1/2 bg-white h-full flex  flex-col pt-6 font-bold pl-4`}>
+                        <ol className={`${menuHamburguer ? '-translate-x-full' : 'translate-x-0'} transition-all ease-linear duration-300 w-1/2 bg-white h-screen flex flex-col pt-6 font-bold pl-4`}>
                             <Image className='relative z-50 md:hidden transition-all ease-in-out duration-1000 m-0 p-0 mr-4' onClick={() => {
                                 setMenuCart(false)
                                 setMenuHamburguer(!menuHamburguer)
